b9a1f96b735663eb150c3f26ed128a21d9f193b8
Alberto <albertodangelo@outlook.com>
2020-04-29 15:37:15 +0200

    footer geändert

--- wp-content/themes/yoga-im-zentrum/footer.php
+++ wp-content/themes/yoga-im-zentrum/footer.php
@@ -13,7 +13,7 @@
 	
 	
     <div class="container container-zertifikat">
-        <img class="zertifikat" src="<?php  echo get_template_directory_uri() ?>/images/zertifikat-iyengar.png" >
+    
     </div>
 
     
@@ -21,20 +21,22 @@
     <footer>  
           <div class="container footer">
             <div class="row">
-              <div class="col-lg-3 col-md-6 col-sm-6"><p class="school">IYENGAR-YOGA-IM-ZENTRUM<br />Cardiance Clinic</p>
+              <div class="col-lg-3 col-md-6 col-sm-6"><p class="school">Yogazentrum Pfäffikon SZ
+              <br />Dr. Kerstin Khattab
+              <br />Cardiance Clinic</p>
         </div>
             <div class="col-lg-3 col-md-6  col-sm-6">
           <p>                 
             Zentrum Staldenbach 5/7<br />
             8808 Pfäffikon SZ / Schweiz<br />
-            www.cardiance.com<br />
+            <a target="_blank" href="https://www.cardiance.com">www.cardiance.com</a><br />
             052-303 47 47
           </p>
           </div>
             <div class="col-lg-3 col-md-6  col-sm-6">
               <p>
-                Die Yogaschule<br />   
-            IYENGAR-YOGA-IM-ZENTRUM<br />
+                Das Yogazentrum<br />   
+            Dr. Kerstin Khattab<br />
             befindet sich in den Räumlichkeiten<br />
             der  CARDIANCE CLINIC
           </p>
@@ -43,7 +45,7 @@
             <p>
               Informationen zu Yogatherapie<br /> 
             und Anmeldung:<br />
-            <a href="mailto:info@iyengar-yoga-im-zentrum.ch">info@iyengar-yoga-im-zentrum.ch</a><br />
+            <a href="mailto:info@dr-kerstin-khatta.yoga">info@dr-kerstin-khatta.yoga</a><br />
             079-741 97 09
             </p>
 
